92fd077528e8c558f4f3dc821a9bf6c12c3d649a
Shubham <Singhalshubham610@gmail.com>
2022-04-09 20:55:41 +0530

    initialcommit

---
+++ duplicateInArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+
+public class duplicateInArray {
+    public static void main(String[] args) {
+        int arr[] = {20,10,50,30,40,50,10,};
+        for(int i=0;i<arr.length;i++){
+            arr[arr[i] % arr.length] = arr[arr[i] % arr.length] + arr.length;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] >=2*arr.length){
+                System.out.print(i +" ");
+            }
+        }
+    }
+}
