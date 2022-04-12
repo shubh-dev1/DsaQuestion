d36415fcace51e5e5821f52c745964783e3ce37e
Shubham <Singhalshubham610@gmail.com>
2022-04-13 01:25:40 +0530

    initialcommit

---
+++ kadaneAlgoContiguousSubArraySum.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class kadaneAlgoContiguousSubArraySum {
+    static void maxSumArray(int arr[]) {
+        int maxsum = Integer.MIN_VALUE;
+        int cursum = 0;
+        for (int i = 0; i <= arr.length-1; i++) {
+            cursum = cursum + arr[i];
+            if (cursum > maxsum) {
+                maxsum = cursum;
+            }
+            if (cursum < 0) {
+                cursum = 0;
+            }
+
+        }
+        System.out.println(maxsum);
+    }
+
+
+    public static void main (String[]args){
+        int arr[] = {-2, -3, 4, -1, -2, 1, 5 ,-3};
+        maxSumArray(arr);
+    }
+}
