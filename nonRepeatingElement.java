580d8ec6221853f0fc21680ec1596ee96ed65a4a
Shubham <Singhalshubham610@gmail.com>
2022-04-18 23:21:50 +0530

    initilacommit

---
+++ nonRepeatingElement.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class nonRepeatingElement {
+    public static void main(String[] args) {
+        int arr[] = {1,5,5,1,7,7};
+        int n= arr.length;
+        for(int i=0;i<n;i++){
+            int j=0;
+            for(j=0;j<n;j++){
+                if(i!=j && arr[i] ==arr[j]){
+                    break;
+                }
+
+
+            }
+            if(j == n){
+                System.out.println(arr[i] +" ");
+                return;
+            }
+        }
+        System.out.println("-1");
+    }
+}
