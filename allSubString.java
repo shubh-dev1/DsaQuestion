a77e29671a1bd5ee65596635042a25ed7de7e963
Shubham <Singhalshubham610@gmail.com>
2022-05-10 23:10:17 +0530

    initialcommit

---
+++ allSubString.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class allSubString {
+    public static void main(String[] args) {
+        String str ="abccd";
+        int cnt =0;
+        for(int i=0;i<str.length();i++){
+            for(int j = i+1;j<=str.length();j++){
+                cnt ++;
+                System.out.println(str.substring(i,j));
+
+            }
+        }
+        System.out.println(cnt);
+    }
+}
