f1637240067ca7a3cabeea187e1fbc9f58fafd36
Shubham <Singhalshubham610@gmail.com>
2022-05-22 23:31:34 +0530

    initialcommit

---
+++ searchInSortedAndRotated.java
@@ -0,0 +1,31 @@
+//package BasicCode.searching;
+//
+//public class searchInSortedAndRotated {
+//    static int bSearch(int arr[] ,int n){
+//        int low =0;
+//        int high =arr.length-1;
+//        while(low<=high){
+//            int mid = (low+high)/2;
+//            if(mid== n){
+//                return mid;
+//            }
+//
+//            if(arr[mid]> arr[low]);
+//        }
+//    }
+//
+//
+//
+//
+//
+//
+//
+//    public static void main(String[] args) {
+//        int arr[] = {20,30,40,50,60,5,10};
+//        int n = 5;
+//        bSearch(arr,n);
+//
+//
+//
+//    }
+//}
