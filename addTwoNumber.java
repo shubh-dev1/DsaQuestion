9f29efc3f9da86e6d325eafc9cb1a28d3b578123
Shubham <Singhalshubham610@gmail.com>
2022-05-27 23:46:31 +0530

    initialcommit

--- addTwoNumber.java
+++ addTwoNumber.java
@@ -2,12 +2,12 @@ package Arrays;
 
 public class addTwoNumber {
     public static void main(String[] args) {
-       int a = 10;
-       int b =20;
-
-        System.out.println(a+b);
-
-        int ans = a+b; // extra variable for sum
-        System.out.println(ans);
+//       int a = 10;
+//       int b =20;
+//
+//        System.out.println(a+b);
+//
+//        int ans = a+b; // extra variable for sum
+        System.out.println(10^12);
     }
 }
