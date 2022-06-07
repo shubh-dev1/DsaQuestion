3dc4ac30c6a0baaa3bd00b45f313901057972ddb
Shubham <Singhalshubham610@gmail.com>
2022-06-07 23:23:57 +0530

    initialcommit

---
+++ highestFrequencyCharacter.java
@@ -0,0 +1,31 @@
+package BasicCode.Hashmaps;
+
+import java.util.*;
+
+public class highestFrequencyCharacter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        HashMap<Character,Integer> hm = new HashMap<>();
+
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(hm.containsKey(ch)){
+                int of = hm.get(ch);
+                int nf = of+1;
+                hm.put(ch,nf);
+            }else{
+                hm.put(ch,1);
+            }
+        }
+
+        char mfc =str.charAt(0);
+        for(Character key : hm.keySet()){
+            if(hm.get(key) >hm.get(mfc)){
+                mfc = key;
+            }
+        }
+        System.out.print("Highest Frequency Character is:");
+        System.out.println(mfc);
+    }
+}
